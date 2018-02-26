30b68e4c3390c24b918fea03f121186365d77351
ladhari <amin.ladhari@gmail.com>
2018-02-26 17:52:35 +0100

    add other function

--- dataFiled.php
+++ dataFiled.php
@@ -17,6 +17,27 @@ public function getDailymotionData(){
  return $this->url;
 }
 
+public function getInstagramData(){
+ return $this->url;
+}
+
+
+public function getFacebokData(){
+ return $this->url;
+}
+
+
+public function getTwitterData(){
+ return $this->url;
+}
+
+
+public function getData(){
+ return $this->url;
+}
+
+
+
 
 
 }
